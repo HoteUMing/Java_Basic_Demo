a8f3bc441985c6d79d65512f4eb5a266de3088c3
HoteUMing <1104234446@qq.com>
2021-04-08 10:33:32 +0800

    modify Test_07_5_HashMap.java

--- src/Test_07_5_HashMap.java
+++ src/Test_07_5_HashMap.java
@@ -1,9 +1,40 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Test_07_5_HashMap {
     public void start() {
         HashMap<Integer, String> map1 = new HashMap<>();
         HashMap<Integer, String> map2 = new HashMap<>(20);
+
+        map2.put(0, "hello");
+        map2.put(1, "world");
+        map2.put(2, "key");
+        map2.put(3, "value");
+
+        //遍历HashMap的方法-1
+        Set<Integer> keys = map2.keySet();
+        Iterator<Integer> iterator1 = keys.iterator();
+        while (iterator1.hasNext()) {
+            Integer key = iterator1.next();
+            System.out.println("map2.keySet(): " + key + " = " + map2.get(key));
+            //map2.keySet(): 0 = hello
+            //map2.keySet(): 1 = world
+            //map2.keySet(): 2 = key
+            //map2.keySet(): 3 = value
+        }
+        //遍历HashMap的方法-2
+        Set<Map.Entry<Integer, String>> entrys = map2.entrySet();
+        Iterator<Map.Entry<Integer, String>> iterator2 = entrys.iterator();
+        while (iterator2.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator2.next();
+            System.out.println("map2.entrySet(): " + entry.getKey() + " = " + entry.getValue());
+            //map2.entrySet(): 0 = hello
+            //map2.entrySet(): 1 = world
+            //map2.entrySet(): 2 = key
+            //map2.entrySet(): 3 = value
+        }
 
         String put = map1.put(0, "a");
         System.out.println("put(0, \"a\"): " + put);
